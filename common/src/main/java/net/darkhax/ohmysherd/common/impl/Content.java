97ad9e37601779738640054cb974a6d59f518dc6
Tyler Hancock <darklime@live.ca>
2025-10-16 15:59:57 -0600

    Update build dependencies.

--- common/src/main/java/net/darkhax/ohmysherd/common/impl/Content.java
+++ common/src/main/java/net/darkhax/ohmysherd/common/impl/Content.java
@@ -1,28 +1,28 @@
 package net.darkhax.ohmysherd.common.impl;
 
-import net.darkhax.bookshelf.common.api.registry.IContentProvider;
-import net.darkhax.bookshelf.common.api.registry.register.Register;
-import net.darkhax.bookshelf.common.api.registry.register.RegisterPotPatterns;
+import net.darkhax.bookshelf.common.api.registry.ContentProvider;
+import net.darkhax.bookshelf.common.api.registry.adapters.GameRegistryAdapter;
+import net.darkhax.bookshelf.common.impl.registry.adapter.PotPatternAdapter;
 import net.minecraft.world.item.Item;
 
-public class Content implements IContentProvider {
+public class Content implements ContentProvider {
 
     @Override
-    public String contentNamespace() {
+    public String namespace() {
         return OhMySherd.MOD_ID;
     }
 
     @Override
-    public void registerItems(Register<Item> registry) {
+    public void defineItems(GameRegistryAdapter<Item> registry) {
         for (SherdType type : SherdType.values()) {
             registry.add(type.itemId(), type.item());
         }
     }
 
     @Override
-    public void registerPotPatterns(RegisterPotPatterns registry) {
+    public void definePotPatterns(PotPatternAdapter registry) {
         for (SherdType type : SherdType.values()) {
-            registry.add(type.item(), type.patternId());
+            registry.addWithItem(type.patternId(), type.item());
         }
     }
 }
